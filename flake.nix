b368132effd614027dfabc420b8bb530b04a61c6
Vir-Suppae <vir.suppae@gmail.com>
2026-08-26 16:41:56 -0500

    added flake

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "A very basic python dev flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = inputs: {
+    devShells = builtins.mapAttrs (system: pkgs: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          python315
+        ];
+        shellHook = ''
+          echo "Welcome to the dev shell ig :P"
+        '';
+      }
+    }) inputs.nixpkgs.legacyPackages;
+  };
+}
